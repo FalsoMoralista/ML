63271a2680e276cd9520a5545bc5f7aaa8524a4f
FalsoMoralista <lucianoadfilho@gmail.com>
2025-02-25 21:03:10 -0300

    update

--- SVD/svd_whitening.py
+++ SVD/svd_whitening.py
@@ -12,4 +12,13 @@ print('Tensor:', X.size())
 svd_helper = SVD_Helper(m=X.size(0), n=X.size(1))
 svd_helper.train(X=X, no_steps=10000)
 
+U, S, V_T = torch.linalg.svd(X)
+
+u, s, v_T = svd_helper.SVD()
+
+print('difference:', torch.linalg.matrix_norm(U - u, ord='fro'))
+print('difference:', torch.linalg.matrix_norm(S - s, ord='fro'))
+print('difference:', torch.linalg.matrix_norm(V_T - v_T, ord='fro'))
+
+
 
